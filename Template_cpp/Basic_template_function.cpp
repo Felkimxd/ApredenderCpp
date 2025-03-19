8b6ae8b71de1f4a23c8d6ccd081ba5819f2b3120
Felkim <fipojaq2005@gmail.com>
2025-03-19 09:16:37 -0500

    Template basics

---
+++ Template_cpp/Basic_template_function.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+T maxValue(T a, T b)
+{
+    return (a < b) ? a : b;
+}
+
+int main()
+{
+    cout << maxValue(51, 59) << endl;                             // Should print 10
+    cout << maxValue(3.5, 2.1) << endl;                          // Should print 3.5
+    cout << maxValue(string("pineaple"), string("orange")) << endl; // Should print "orange"
+
+}
---
+++ Template_cpp/Overload_template.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+T maxValue(T a, T b)
+{
+    return (a > b) ? a : b;
+}
+
+template <typename T>
+T maxValue(T a, T b, T c)
+{
+    return maxValue(a, maxValue(b, c));
+}
+
+int main()
+{
+    cout << maxValue(8, 2, 1) << endl;       // Should print 5
+    cout << maxValue(4.5, 6.9, 0.9) << endl; // Should print 3.7
+}
