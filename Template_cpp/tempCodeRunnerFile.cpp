6b8f7e19c13b575be7cdaa825731e650cb72de68
Felkim <fipojaq2005@gmail.com>
2025-03-28 20:53:50 -0500

    COSAS DE TEMPALTE

--- Template_cpp/tempCodeRunnerFile.cpp
+++ Template_cpp/tempCodeRunnerFile.cpp
@@ -1,7 +1,1 @@
-#include <iostream>
-#include <concepts>
-
-using namespace std;
-
-template <typename T>
-concept Numeric = std::integral<T> || std::floating_point
+cout << "=================== Overload a Template Function ==============================" << endl;
